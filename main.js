8c7e6cf74f9bdcc786276dd9fb67669395e8775f
eunni <choidy0106@gmail.com>
2024-04-17 20:45:17 +0900

    second

--- main.js
+++ main.js
@@ -14,19 +14,19 @@ function reset() {
   document.body.style.backgroundColor = 'linen'
 }
 function showbb() {
-    document.getElementById('fig').src = 'img/baseball.jpeg'
+    document.getElementById('fig').src = 'img/basball.png'
     document.getElementById('desc').innerHTML =
       '⚾︎야구를 사랑할까말까⚾︎'
   }
   
   function showmv() {
-    document.getElementById('fig').src = 'img/movie.jpeg'
+    document.getElementById('fig').src = 'img/movie.png'
     document.getElementById('desc').innerHTML =
       '👀영화 보는거 좋아해요.👀'
   }
   
   function showms() {
-    document.getElementById('fig').src = 'img/music.jpg'
+    document.getElementById('fig').src = 'img/music.png'
     document.getElementById('desc').innerHTML =
       '🎧︎음악듣기 좋아해요.🎧︎ '
   }
